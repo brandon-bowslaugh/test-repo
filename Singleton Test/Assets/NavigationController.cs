8f2bfd461ccd595d6928e6851dd908bc482e526f
brandon-bowslaugh <branbowslaugh@gmail.com>
2018-08-06 19:57:03 -0400

    test

---
+++ Singleton Test/Assets/NavigationController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationController : MonoBehaviour {
+
+    #region Create Singleton
+    private static NavigationController _instance;
+
+    public static NavigationController Instance {
+        get {
+            if (_instance == null) {
+                GameObject go = new GameObject( "NavigationController" );
+                go.AddComponent<NavigationController>();
+            }
+            return _instance;
+        }
+    }
+
+    void Awake() {
+        _instance = this;
+    }
+    #endregion
+
+    #region Movement Variables
+    private static int _movementRemaining = 6;
+    private static GridLayout gridLayout;
+    private static int _moveSpaces = 2;
+
+    public static int MovementRemaining {
+        get {
+            return _movementRemaining;
+        }
+    }
+
+    public static int MoveSpaces {
+        get {
+            return _moveSpaces;
+        }
+        set {
+            _moveSpaces = value;
+        }
+    }
+    #endregion
+
+    #region Movement Functionality
+    public void Init() {
+    }
+    public void Move() {
+        _movementRemaining = _movementRemaining - _moveSpaces;
+    }
+    public void CalcMoveArea() {
+
+    }
+    #endregion
+}
---
+++ Singleton Test/Assets/player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log( "Movement Available: " + NavigationController.MovementRemaining );
+        Debug.Log( "Move " + NavigationController.MoveSpaces + " spaces." );
+        NavigationController.Instance.Move();
+        Debug.Log( "Moving..." );
+        Debug.Log( "Moved " + NavigationController.MoveSpaces + " spaces." );
+        Debug.Log( "Movement Remainging: " + NavigationController.MovementRemaining );
+
+        Debug.Log( "" );
+        Debug.Log( "Moving Again" );
+        NavigationController.MoveSpaces = 3;
+        Debug.Log( "Movement Available: " + NavigationController.MovementRemaining );
+        Debug.Log( "Move " + NavigationController.MoveSpaces + " spaces." );
+        NavigationController.Instance.Move();
+        Debug.Log( "Moving..." );
+        Debug.Log( "Moved " + NavigationController.MoveSpaces + " spaces." );
+        Debug.Log( "Movement Remainging: " + NavigationController.MovementRemaining );
+
+    }
+}
